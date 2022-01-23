dfe296c75533f76e35f3bd4c051657f9bf520b63
Corey J <corey@roll20.net>
2022-01-23 16:26:26 -0500

    Canvas Styling

--- js/main.js
+++ js/main.js
@@ -6,7 +6,30 @@ if (location.hostname === ""){
      * https://doc.babylonjs.com/toolsAndResources/assetLibraries/availableTextures */
     textureURL = 'https://www.babylonjs-playground.com/';
 }
-    
+// Generate the Canvas
+const CANVAS_WIDTH = 1920;
+const CANVAS_HEIGHT = 1080;
+const ratio = CANVAS_WIDTH / CANVAS_HEIGHT;
+
+function scaletosmallest(ratio) {
+    if ( (windowWidth() / ratio) <= windowWidth() ) {
+        
+        const canvasEle = document.querySelector("#gamecontainer");
+        canvasEle.style.width = '99%';
+        canvasEle.style.height = 'auto';
+    } else {
+        canvasEle.style.height = '99%';
+        canvasEle.style.width = 'auto';
+    }
+  }
+
+function windowWidth() {
+    var docElemProp = window.document.documentElement.clientWidth,
+        body = window.document.body;
+    return window.document.compatMode === "CSS1Compat" && docElemProp || body && body.clientWidth || docElemProp;
+}
+
+
 
 
 
@@ -14,7 +37,7 @@ const createScene = function() {
     const scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3.Black;
 
-
+    window.onresize = scaletosmallest(ratio);
 
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
 
@@ -27,7 +50,7 @@ const createScene = function() {
     const skybox = new Skybox(scene);
 
     const ObsFactory = new ObstacleFactory(scene);
-
+    const hud = new HUD();
 
     const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:50, height:10} , scene); 
 
@@ -38,6 +61,8 @@ const createScene = function() {
      //set physics models to objects made
     player.mesh.physicsImpostor = new BABYLON.PhysicsImpostor(player.mesh, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
     ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+
+   
 
     return scene;
 };
