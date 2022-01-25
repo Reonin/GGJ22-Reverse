f47f6971b4c92cb056b99f78143cad677fa67a2c
ttilley02 <56703674+ttilley02@users.noreply.github.com>
2022-01-24 22:02:03 -0500

    moon phase

--- js/moon.js
+++ js/moon.js
@@ -1,26 +1,50 @@
 /**
  * Creates Moon Object that will be clickable to change the player object
  */
- class Moon {
-    constructor(scene){
-        const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 4, segments: 48});
-        sphere.position.x = -25;
-        sphere.position.y = 16;
+class Moon {
+  constructor(scene) {
+    const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {
+      diameter: 4,
+      segments: 32,
+    });
+    sphere.position.x = -20;
+    sphere.position.y = 17;
 
+    const sphereMaterial = new BABYLON.StandardMaterial('material', scene);
+    sphereMaterial.diffuseTexture = new BABYLON.Texture(
+      textureURL.concat('textures/sphereMap.png'),
+      scene
+    );
+    sphere.material = sphereMaterial;
 
-        const sphereMaterial = new BABYLON.StandardMaterial("material", scene);
-        sphereMaterial.diffuseTexture = new BABYLON.Texture(textureURL.concat("textures/sphereMap.png"), scene);
-        sphere.material = sphereMaterial;
+    this.mesh = sphere;
 
-        this.mesh = sphere;
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+    this.mesh.actionManager = new BABYLON.ActionManager(scene);
+    this.mesh.actionManager
+    
+    // FADE OUT MOON
+      .registerAction(
+        new BABYLON.InterpolateValueAction(
+          BABYLON.ActionManager.OnPickTrigger,
+          light,
+          'diffuse',
+          BABYLON.Color3.Black(),
+          1000
+        )
+      )
 
-        return this;
-    }
+    // UNFADE MOON
+      .then(
+        new BABYLON.InterpolateValueAction(
+          BABYLON.ActionManager.OnPickTrigger,
+          light,
+          'diffuse',
+          BABYLON.Color3.White(),
+          1000
+        )
+      );
 
-    mesh = {};
-
-    phaseChange = function() {
-        // color settings to go here
-        console.log("phase change happened spooky!")
-    }
+    return this;
+  }
 }
